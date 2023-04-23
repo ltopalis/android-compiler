37a6ee81606618dc69d910f64b161ffef1c74309
Lazaros Topalis <lazarostop32@gmail.com>
2023-04-23 17:56:09 +0300

    update lexer with yyerror and more regex

--- lexer.h
+++ lexer.h
@@ -4,18 +4,18 @@
 /*           SYMBOLS            */
 
 #define T_EQUAL                     1
-#define T_START_COMMENT             2
-#define T_END_COMMENT               3
+#define T_COMMENT                   2
+#define T_SYMBOL                    3
 #define T_SEMICOLON                 4
 #define T_END_ONE_LINE_ELEM         5
 #define T_END_MANY_LINES_ELEM       6
 #define T_QUOTATION                 7
 #define T_UNDERSCORE                8
 #define T_DOUBLE_MINUS              9
-#define T_SYMBOL                    10
 
 /*           ELEMENTS           */
-   
+
+#define T_PROGRESS_BAR_S            10
 #define T_TEXT_VIEW_S               11  
 #define T_RADIO_BUTTON_S            12
 #define T_RELATIVE_LAYOUT_S         13
@@ -23,12 +23,12 @@
 #define T_BUTTON_S                  15   
 #define T_LINEAR_LAYOUT_F           16 
 #define T_RELATIVE_LAYOUT_F         17
-#define T_RADIO_GROUP_F             18  
-#define T_PROGRESS_BAR_S            19
+#define T_RADIO_GROUP_F             18
+#define T_RADIO_GROUP_S             19
+#define T_LINEAR_LAYOUT_S           20  
 
 /*          ATTRIBUTES          */
-    
-#define T_PROGRESS                  20  
+  
 #define T_ANDROID                   21  
 #define T_LAYOUT_HEIGHT             22  
 #define T_LAYOUT_WIDTH              23  
@@ -39,12 +39,15 @@
 #define T_PADDING                   28  
 #define T_TEXT_COLOR                29  
 #define T_CHECKED_BUTTON            30 
-#define T_MAX                       31 
+#define T_MAX                       31
+#define T_PROGRESS                  32
 
 /*            OTHER             */
   
-#define T_NUMBER                    32
-#define T_ALPHARITHMETIC            33  
+#define T_ALPHANUMERIC              33  
+#define T_NUMBER                    34
+
+#define T_ERROR                     100
 #define T_EOF                       0
 
 
