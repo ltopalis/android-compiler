b84c63666025b0c28523e29c6fe96f6294f3185c
Lazaros Topalis <lazarostop32@gmail.com>
2023-04-25 18:38:46 +0300

    creating sub-lectical-analysis

--- lexer.h
+++ lexer.h
@@ -10,44 +10,42 @@
 #define T_END_ONE_LINE_ELEM         5
 #define T_END_MANY_LINES_ELEM       6
 #define T_QUOTATION                 7
-#define T_UNDERSCORE                8
-#define T_DOUBLE_MINUS              9
 
 /*           ELEMENTS           */
 
-#define T_PROGRESS_BAR_S            10
-#define T_TEXT_VIEW_S               11  
-#define T_RADIO_BUTTON_S            12
-#define T_RELATIVE_LAYOUT_S         13
-#define T_IMAGE_VIEW_S              14  
-#define T_BUTTON_S                  15   
-#define T_LINEAR_LAYOUT_F           16 
-#define T_RELATIVE_LAYOUT_F         17
-#define T_RADIO_GROUP_F             18
-#define T_RADIO_GROUP_S             19
-#define T_LINEAR_LAYOUT_S           20  
+#define T_PROGRESS_BAR_S            8
+#define T_TEXT_VIEW_S               9  
+#define T_RADIO_BUTTON_S            10
+#define T_RELATIVE_LAYOUT_S         11
+#define T_IMAGE_VIEW_S              12  
+#define T_BUTTON_S                  13   
+#define T_LINEAR_LAYOUT_F           14 
+#define T_RELATIVE_LAYOUT_F         15
+#define T_RADIO_GROUP_F             16
+#define T_RADIO_GROUP_S             17
+#define T_LINEAR_LAYOUT_S           18  
 
 /*          ATTRIBUTES          */
   
-#define T_ANDROID                   21  
-#define T_LAYOUT_HEIGHT             22  
-#define T_LAYOUT_WIDTH              23  
-#define T_ORIENTATION               24 
-#define T_ID                        25
-#define T_TEXT                      26 
-#define T_SRC                       27 
-#define T_PADDING                   28  
-#define T_TEXT_COLOR                29  
-#define T_CHECKED_BUTTON            30 
-#define T_MAX                       31
-#define T_PROGRESS                  32
+#define T_ANDROID                   19  
+#define T_LAYOUT_HEIGHT             20  
+#define T_LAYOUT_WIDTH              21  
+#define T_ORIENTATION               22 
+#define T_ID                        23
+#define T_TEXT                      24 
+#define T_SRC                       25 
+#define T_PADDING                   26  
+#define T_TEXT_COLOR                27  
+#define T_CHECKED_BUTTON            28 
+#define T_MAX                       29
+#define T_PROGRESS                  30
 
 /*            OTHER             */
   
-#define T_ALPHANUMERIC              33  
-#define T_NUMBER                    34
+#define T_ALPHANUMERIC              31
+#define T_NUMBER                    32
+#define T_VTEXT                     33
 
-#define T_ERROR                     100
 #define T_EOF                       0
 
 
