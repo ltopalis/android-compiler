da8d98b6067d8dd6760acfb6a2f0064388880a1a
Lazaros Topalis <lazarostop32@gmail.com>
2023-04-27 15:48:07 +0300

    print errors

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -3,7 +3,6 @@
 
 from itertools import permutations
 
-
 def myPermutations(list_of_elements, first=False):
     perm = list(permutations(list_of_elements))
     tabs = "\t" * 8
@@ -24,11 +23,36 @@ def myPermutations(list_of_elements, first=False):
         print(elem, end=" ")
     print()
 
+def myPermutationsWithErrors(list_of_elements, message, first=False):
+    perm = list(permutations(list_of_elements))
+    tabs = '\t'*7 + "  "
 
-ypochreotika = ["layoutWidth", "layoutHeight"]
-id = ypochreotika + ["id"]
+
+    if not first:
+        print(f"{tabs}|  ", end="")
+    for elem in perm[0]:
+        print(f'{elem}', end=" ")
+    print(f'\t\t\t {message} \n{tabs}|  ', end="")
+
+    for index in range(1, len(perm) - 1):
+        for elem in perm[index]:
+            print(elem, end=" ")
+        print(f'\t\t\t {message}\n{tabs}|  ', end="")
+
+    for elem in perm[len(perm) - 1]:
+        print(elem, end=" ")
+    print(f'\t\t\t {message}\n', end="")
 
 
-myPermutations(ypochreotika, first=True)
-myPermutations(id)
+height = ["layoutWidth", "error"]
+width = ["error", "layoutHeight"]
 
+height_id = ["id","layoutWidth", "error"]
+width_id = ["id", "error", "layoutHeight"]
+
+
+myPermutationsWithErrors(height, '{ yyerror("android:layout_height is mandatory"); yyerrok; }', first=False)
+myPermutationsWithErrors(width, '{ yyerror("android:layout_width is mandatory"); yyerrok; }', first=False)
+
+myPermutationsWithErrors(height_id, '{ yyerror("android:layout_height is mandatory"); yyerrok; }', first=False)
+myPermutationsWithErrors(width_id, '{ yyerror("android:layout_width is mandatory"); yyerrok; }', first=False)
